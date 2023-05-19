76d1edf4160ecdf2921920db0405e6688457fce7
yuyonfdong <18616532194@163.com>
2023-05-19 14:55:00 +0800

    1

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -39,19 +39,19 @@
     <div class="home-collapse">
       <div class="home-conent">
         <el-collapse v-model="activeNames" accordion>
-          <el-collapse-item title="一致性 Consistency" name="1">
+          <el-collapse-item :title="$t('home.homeSupporting')" name="1">
             <div>
-              与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。
+              {{ $t('home.homeSupportingContent') }}
             </div>
-            <!-- <div>
-              在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。
-            </div> -->
           </el-collapse-item>
-          <el-collapse-item title="反馈 Feedback" name="2">
-            <div>
-              控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；
-            </div>
-            <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+          <el-collapse-item :title="$t('home.homeCoMpos')" name="2">
+            <div>{{ $t('home.homeCoMposContent') }}</div>
+          </el-collapse-item>
+          <el-collapse-item :title="$t('home.homeInteroper')" name="3">
+            <div>{{ $t('home.homeInteroperContent') }}</div>
+          </el-collapse-item>
+          <el-collapse-item :title="$t('home.homeTranspraent')" name="4">
+            <div>{{ $t('home.homeTranspraentContent') }}</div>
           </el-collapse-item>
         </el-collapse>
       </div>
@@ -1224,6 +1224,9 @@ export default {
           text-align: left;
           color: #adadad;
         }
+      }
+      li:hover {
+        background: rgba(129, 129, 129, 0.2);
       }
     }
     .mobile {
